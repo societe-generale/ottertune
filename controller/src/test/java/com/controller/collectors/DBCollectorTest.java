febb60a0cc9795d2fdcbdd4c60be3ab97e2f76c3
Shuli Jiang <11hifish@gmail.com>
2018-01-17 01:35:34 -0500

    json schema validation experiment

--- controller/src/test/java/com/controller/collectors/DBCollectorTest.java
+++ controller/src/test/java/com/controller/collectors/DBCollectorTest.java
@@ -16,10 +16,36 @@ public class DBCollectorTest {
     @Test
     public void mysqlOutputTest() throws IOException, ProcessingException {
         File schemaFile = new File("/vagrant/ottertune/controller/src/main/java/com/controller/schema.json");
-        File jsonFile = new File("/vagrant/ottertune/controller/output/mysql/knobs.json");
+        File knobsJson = new File("/vagrant/ottertune/controller/output/mysql/knobs.json");
+        File metricsAfterJson = new File("/vagrant/ottertune/controller/output/mysql/metrics_after.json");
+        File metricsBeforeJson = new File("/vagrant/ottertune/controller/output/mysql/metrics_before.json");
 
-        if (!ValidationUtils.isJsonValid(schemaFile, jsonFile)) {
-            throw new InvalidObjectException("invalid json output file");
+        if(!ValidationUtils.isJsonValid(schemaFile, knobsJson)) {
+            throw new InvalidObjectException("invalid knobs json output file");
+        }
+        if(!ValidationUtils.isJsonValid(schemaFile, metricsAfterJson)) {
+            throw new InvalidObjectException("invalid metrics_after json output file");
+        }
+        if(!ValidationUtils.isJsonValid(schemaFile, metricsBeforeJson)) {
+            throw new InvalidObjectException("invalid metrics_before json output file");
+        }
+    }
+
+    @Test
+    public void postgresOutputTest() throws IOException, ProcessingException {
+        File schemaFile = new File("/vagrant/ottertune/controller/src/main/java/com/controller/schema.json");
+        File knobsJson = new File("/vagrant/ottertune/controller/output/postgres/knobs.json");
+        File metricsAfterJson = new File("/vagrant/ottertune/controller/output/postgres/metrics_after.json");
+        File metricsBeforeJson = new File("/vagrant/ottertune/controller/output/postgres/metrics_before.json");
+
+        if(!ValidationUtils.isJsonValid(schemaFile, knobsJson)) {
+            throw new InvalidObjectException("invalid knobs json output file");
+        }
+        if(!ValidationUtils.isJsonValid(schemaFile, metricsAfterJson)) {
+            throw new InvalidObjectException("invalid metrics_after json output file");
+        }
+        if(!ValidationUtils.isJsonValid(schemaFile, metricsBeforeJson)) {
+            throw new InvalidObjectException("invalid metrics_before json output file");
         }
     }
 
