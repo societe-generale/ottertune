519134e0536a3ae40859adde14ac5b90554fc32a
Shuli Jiang <11hifish@gmail.com>
2018-01-29 23:06:21 -0500

    fix gradle for apache cli and fix test case

--- controller/src/test/java/com/controller/MainTest.java
+++ controller/src/test/java/com/controller/MainTest.java
@@ -2,29 +2,27 @@ package com.controller;
 
 import org.junit.Test;
 
-import java.lang.reflect.MalformedParametersException;
-
 /**
  * Test for Main.
  * @author Shuli
  */
 public class MainTest {
-    @Test(expected = MalformedParametersException.class)
+    @Test(expected = NullPointerException.class)
     public void testValidCmdLineArg1() {
         Main.main(new String[]{"-t"});
     }
 
-    @Test(expected = MalformedParametersException.class)
+    @Test(expected = NullPointerException.class)
     public void testValidCmdLineArg2() {
         Main.main(new String[]{"-b","20"});
     }
 
-    @Test(expected = MalformedParametersException.class)
+    @Test(expected = NullPointerException.class)
     public void testValidCmdLineArg3() {
         Main.main(new String[]{"-b","20","-f"});
     }
 
-    @Test(expected = MalformedParametersException.class)
+    @Test(expected = NullPointerException.class)
     public void testValidDB() {
         Main.main(new String[]{"-f","src/test/java/com/controller/mock_config1.json"});
     }
