a969c49d81faf7177a40c0ec9f2b3aa600324811
Shuli Jiang <11hifish@gmail.com>
2018-01-31 15:47:06 -0500

    fix controller test file path

--- controller/src/test/java/com/controller/collectors/DBCollectorTest.java
+++ controller/src/test/java/com/controller/collectors/DBCollectorTest.java
@@ -1,7 +1,9 @@
 package com.controller.collectors;
 
+import com.controller.util.PathUtil;
 import com.controller.util.ValidationUtils;
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
@@ -13,12 +15,31 @@ import java.io.InvalidObjectException;
  * @author Shuli
  */
 public class DBCollectorTest {
+    private String srcPath;
+    private String outputPath;
+    private String schemaFilePath;
+
+    @Before
+    public void setUp() {
+        srcPath = PathUtil.getAbsPath("src");
+        outputPath = PathUtil.getAbsPath("output");
+        schemaFilePath = srcPath + "/main/java/com/controller/json_validation_schema/schema.json";
+    }
+
+    @Test
+    public void pathTest() {
+
+    }
+
     @Test
     public void mysqlOutputTest() throws IOException, ProcessingException {
-        File schemaFile = new File("/vagrant/ottertune/controller/src/main/java/com/controller/json_validation_schema/schema.json");
-        File knobsJson = new File("/vagrant/ottertune/controller/output/mysql/knobs.json");
-        File metricsAfterJson = new File("/vagrant/ottertune/controller/output/mysql/metrics_after.json");
-        File metricsBeforeJson = new File("/vagrant/ottertune/controller/output/mysql/metrics_before.json");
+        String knobsJsonPath = outputPath + "/mysql/knobs.json";
+        String metricsAfterJsonPath = outputPath + "/mysql/metrics_after.json";
+        String metricsBeforeJsonPath = outputPath + "/mysql/metrics_before.json";
+        File schemaFile = new File(schemaFilePath);
+        File knobsJson = new File(knobsJsonPath);
+        File metricsAfterJson = new File(metricsAfterJsonPath);
+        File metricsBeforeJson = new File(metricsBeforeJsonPath);
 
         if(!ValidationUtils.isJsonValid(schemaFile, knobsJson)) {
             throw new InvalidObjectException("invalid knobs json output file");
@@ -33,10 +54,13 @@ public class DBCollectorTest {
 
     @Test
     public void postgresOutputTest() throws IOException, ProcessingException {
-        File schemaFile = new File("/vagrant/ottertune/controller/src/main/java/com/controller/json_validation_schema/schema.json");
-        File knobsJson = new File("/vagrant/ottertune/controller/output/postgres/knobs.json");
-        File metricsAfterJson = new File("/vagrant/ottertune/controller/output/postgres/metrics_after.json");
-        File metricsBeforeJson = new File("/vagrant/ottertune/controller/output/postgres/metrics_before.json");
+        String knobsJsonPath = outputPath + "/postgres/knobs.json";
+        String metricsAfterJsonPath = outputPath + "/postgres/metrics_after.json";
+        String metricsBeforeJsonPath = outputPath + "/postgres/metrics_before.json";
+        File schemaFile = new File(schemaFilePath);
+        File knobsJson = new File(knobsJsonPath);
+        File metricsAfterJson = new File(metricsAfterJsonPath);
+        File metricsBeforeJson = new File(metricsBeforeJsonPath);
 
         if(!ValidationUtils.isJsonValid(schemaFile, knobsJson)) {
             throw new InvalidObjectException("invalid knobs json output file");
@@ -51,9 +75,11 @@ public class DBCollectorTest {
 
     @Test
     public void mockJsonOutputTest() throws IOException, ProcessingException {
-        File schemaFile = new File("/vagrant/ottertune/controller/src/main/java/com/controller/json_validation_schema/schema.json");
-        File mockJsonFile1 = new File("/vagrant/ottertune/controller/src/test/java/com/controller/collectors/mock_output/mockJsonOutput1.json");
-        File mockJsonFile2 = new File("/vagrant/ottertune/controller/src/test/java/com/controller/collectors/mock_output/mockJsonOutput2.json");
+        String mockJsonFile1Path = srcPath + "/test/java/com/controller/collectors/mock_output/mockJsonOutput1.json";
+        String mockJsonFile2Path = srcPath + "/test/java/com/controller/collectors/mock_output/mockJsonOutput2.json";
+        File schemaFile = new File(schemaFilePath);
+        File mockJsonFile1 = new File(mockJsonFile1Path);
+        File mockJsonFile2 = new File(mockJsonFile2Path);
 
         // wrong number of levels for "global"
         if(ValidationUtils.isJsonValid(schemaFile, mockJsonFile1)) {
@@ -67,9 +93,12 @@ public class DBCollectorTest {
 
     @Test
     public void outputSummaryJsonTest() throws IOException, ProcessingException {
-        File schemaFile = new File("/vagrant/ottertune/controller/src/main/java/com/controller/json_validation_schema/summary_schema.json");
-        File mysqlSummary = new File("/vagrant/ottertune/controller/output/mysql/summary.json");
-        File postgresSummary = new File("/vagrant/ottertune/controller/output/postgres/summary.json");
+        String summarySchemaFilePath = srcPath + "/main/java/com/controller/json_validation_schema/summary_schema.json";
+        File schemaFile = new File(summarySchemaFilePath);
+        String mysqlSummaryPath = outputPath + "/mysql/summary.json";
+        String postgresSummaryPath = outputPath + "/postgres/summary.json";
+        File mysqlSummary = new File(mysqlSummaryPath);
+        File postgresSummary = new File(postgresSummaryPath);
 
         if(!ValidationUtils.isJsonValid(schemaFile, mysqlSummary)) {
             throw new InvalidObjectException("mysql json output summary file is invalid!");
